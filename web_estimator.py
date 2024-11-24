6c079a1c1217ae6f2e564e8ef6db3770575c7379
ngcarvajall <ngcarvajall@gmail.com>
2024-11-25 00:54:36 +0100

    readme

--- web_estimator.py
+++ web_estimator.py
@@ -27,11 +27,11 @@ st.image(
 
 # Cargar los modelos y transformadores entrenados
 def load_models():
-    with open('modelos/target_encoder.pkl', 'rb') as f:
+    with open('modelos_pickel/target_encoder.pkl', 'rb') as f:
         target_encoder = pickle.load(f)
-    with open('modelos/scaler.pkl', 'rb') as f:
+    with open('modelos_pickel/scaler.pkl', 'rb') as f:
         scaler = pickle.load(f)
-    with open('modelos/random_forest_model.pkl', 'rb') as f:
+    with open('modelos_pickel/random_forest_model.pkl', 'rb') as f:
         model = pickle.load(f)
     return target_encoder, scaler, model
 
